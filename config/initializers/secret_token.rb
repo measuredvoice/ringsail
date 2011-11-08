35323e7594805294d6181d8c504258f240e24ade
Chris Radcliff <chris.radcliff@gmail.com>
2011-11-08 12:18:58 -0800

    Added basic application structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ringsail::Application.config.secret_token = '77f3e4c4ba8927b33e85fd07da27a86f7bd5019a836ceb05ec63cf185f3dbaf9d8d841f208f14fb3cc245457bad0a8672389c96d92ea1903bc9115061ef84766'
