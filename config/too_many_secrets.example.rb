576661525d2a59ab4149175e84071bd1992053d9
Chris Radcliff <chris.radcliff@gmail.com>
2011-12-14 09:22:22 -0800

    Finished getting secrets in their proper place.

---
+++ config/too_many_secrets.example.rb
@@ -0,0 +1,7 @@
+ENV['SENDGRID_USER'] = 'your sendgrid username'
+ENV['SENDGRID_PASSWORD'] = 'your sendgrid password'
+ENV['SENDGRID_DOMAIN'] = 'your sendgrid domain'
+ENV['YAHOO_APP_ID'] = 'your app ID for Yahoo! GeoPlanet'
+ENV['RAILS_COOKIE_TOKEN'] = 'at least 30 random hex digits'
+ENV['EMAIL_BYPASS_USER'] = 'a developer email to use in development'
+
