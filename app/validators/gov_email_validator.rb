acc6decfb1adf558956067d477f00423a7500ba8
Chris Radcliff <chris.radcliff@gmail.com>
2011-12-14 10:41:15 -0800

    Added admins and .mil to the list of valid auth email addresses.

--- app/validators/gov_email_validator.rb
+++ app/validators/gov_email_validator.rb
@@ -1,5 +1,13 @@
 class GovEmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    record.errors.add attribute, "must be a .gov email" unless value =~ /\A[\w+\-.]+@[a-z\d\-.]+\.gov\z/i
+  def validate_each(record, attribute, email_address)
+    record.errors.add attribute, "must be a .gov or .mil email address" unless gov_address?(email_address) || admin_user?(email_address)
+  end
+  
+  def admin_user?(email_address)
+    !User.find_by_email(email_address).nil?
+  end
+
+  def gov_address?(email_address)
+    email_address =~ /\A[\w+\-.]+@[a-z\d\-.]+\.(gov|mil)\z/i
   end
 end
