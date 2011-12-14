a83f7776c7fe199787d083f450b8b2f332ccc219
Chris Radcliff <chris.radcliff@gmail.com>
2011-12-14 13:46:59 -0800

    Added auto-complete fields code for agencies, tags, and language.

---
+++ app/assets/javascripts/outlets.js
@@ -0,0 +1,71 @@
+(function() {
+	// declare default languages array
+	var defaultLanguages = {languages: [
+							{value: "English", name: "English"},
+							{value: "Spanish", name: "Spanish"},
+							{value: "Mandarin", name: "Mandarin"},
+							{value: "Arabic", name: "Arabic"},
+							{value: "Hindu-Urdu", name: "Hindu-Urdu"},
+							{value: "Russian", name: "Russian"},
+							{value: "French", name: "French"}
+						]};
+	
+	// wait for DOM ready to fire the following
+	$(document).ready(function(){
+		
+		// active "Chosen" plugin - Agencies dropdown
+		$(".chzn-select").chosen();
+		
+		// activate the AutoSuggest for languages
+		var languagePlaceholder = $("#language").attr("placeholder");
+		var langPrefillString = $("#language").val();
+		if (langPrefillString){
+			var langPrefills = langPrefillString.split(",");
+			var langPrefillArray = [];
+			for(i=0;i<=langPrefills.length-1;i++){
+				var preObj = {}
+				preObj = {"value": langPrefills[i], "name":langPrefills[i]};
+				langPrefillArray[i] = preObj;
+			}
+		}
+		$("#language").autoSuggest(defaultLanguages.languages, {preFill: langPrefillArray, asHtmlID: "language", minChars: 1, startText:languagePlaceholder, selectionLimit: 1, resultsHighlight: false, neverSubmit:true, selectedItemProp: "name", searchObjProps: "value"});
+		
+		// activate the AutoSuggest for tags
+		//DEV ONLY: 
+		//var jsonTagEndpoint = "add-form_files/tags.php";
+		// LIVE (script automagically adds ?q= for typed query
+		var jsonTagEndpoint = "/tags.json";
+		var tagPlaceholder = $("#tags").attr("placeholder");
+		var prefillString = $("#tags").val();
+		if (prefillString){
+			var prefills = prefillString.split(",");
+			var prefillArray = [];
+			for(i=0;i<=prefills.length-1;i++){
+				var preObj = {}
+				preObj = {"tag_id": prefills[i], "tag_text":prefills[i]};
+				prefillArray[i] = preObj;
+			}
+		}
+		$("#tags").autoSuggest(jsonTagEndpoint, {preFill: prefillArray, asHtmlID:"tags",minChars: 2, selectionLimit: false, startText:tagPlaceholder, resultsHighlight: false, neverSubmit:true, selectedItemProp: "tag_text", selectedValuesProp:"tag_id",searchObjProps: "tag_id"});
+		
+		// when the form is submitted, grab the values from the AutoSuggest fields and pop them into a more friendly-named input 
+		$("form.validate").submit(function(){
+			var destinationFields = Array("tags","language");
+			// loop through desired endpoints
+			for (i=0;i<=destinationFields.length-1;i++){
+				var fieldName = destinationFields[i];
+				var fieldValue = $("#as-values-"+fieldName).val();
+				// does this input already exist in the DOM (and, thus, will be submitted as a POST var)
+				if ($("#"+fieldName).length){
+					// already exists, so lets just pop the right value in there
+					$("#"+fieldName).val(fieldValue);
+				} else {
+					$("<input />").attr({"type":"hidden","name":fieldName}).val(fieldValue).appendTo($(this));
+				}
+			}
+		});
+		
+	});
+	
+	
+}).call(this);
