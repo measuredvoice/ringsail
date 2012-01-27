4d9826c612b75dc73aa6d444ca1d9fcd733bade4
Jed Sundwall <jed@measuredvoice.com>
2012-01-27 10:28:57 -0800

    Added Foursquare and Livestream plugins

---
+++ lib/services/foursquare_service.rb
@@ -0,0 +1,36 @@
+class FoursquareService < Service
+  def self.handles?(uri)
+    uri.host =~ /foursquare.com$/
+  end
+  
+  def self.shortname
+    :foursquare
+  end
+  
+  def display_name
+    "#{account} on Foursquare"
+  end
+
+  def account
+      /\/(?<account>[\w-]+)$/ =~ @uri.path
+    account
+  end
+  
+  def service_url_example
+    "http://www.foursquare.com/NASAJPL"
+  end
+  
+  def service_url_canonical
+    "http://foursquare.com/#{account}"
+  end
+
+  private
+  
+  def fetch_details
+    {
+      :account => account,
+    }
+  end
+end
+
+Service.register(:foursquare, FoursquareService)
---
+++ lib/services/livestream_service.rb
@@ -0,0 +1,36 @@
+class LivestreamService < Service
+  def self.handles?(uri)
+    uri.host =~ /livestream.com$/
+  end
+  
+  def self.shortname
+    :livestream
+  end
+  
+  def display_name
+    "#{account} on Livestream"
+  end
+
+  def account
+      /\/(?<account>[\w-]+)$/ =~ @uri.path
+    account
+  end
+  
+  def service_url_example
+    "http://www.livestream.com/NASAJPL"
+  end
+  
+  def service_url_canonical
+    "http://livestream.com/#{account}"
+  end
+
+  private
+  
+  def fetch_details
+    {
+      :account => account,
+    }
+  end
+end
+
+Service.register(:livestream, LivestreamService)
