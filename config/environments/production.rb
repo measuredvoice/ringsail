5f46b5d492bab1e8d66944795bb3af7d70d2ffe9
Chris Radcliff <chris.radcliff@gmail.com>
2012-03-22 14:12:41 -0700

    Fixed a precompilation error.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Ringsail::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( embed.css gobierno.css howto.css usagov.css )
+  config.assets.precompile += %w( embed.css gobierno.css howto.css usagov.css rails_admin/rails_admin.css rails_admin/rails_admin.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
