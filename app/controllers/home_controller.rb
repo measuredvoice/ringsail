e73ee6ba6abdfff8c66cf5092e7f59aa5a2ddba7
Chris Radcliff <chris.radcliff@gmail.com>
2012-04-12 14:05:55 -0700

    Changed home page redirect. Consolidated links to documentation.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @page_title = "Social Media Registry API"
-    redirect_to rails_admin_path
+    redirect_to ENV['REGISTRY_DOCS_URL'] || 'http://usa.gov/'
   end
 end
