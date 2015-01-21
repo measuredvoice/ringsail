d8fe6b7e07559352725e611da252a6760a7304da
Justin Raines <justin.magtheridon@gmail.com>
2015-01-21 14:05:02 -0500

    add another chart

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -19,10 +19,22 @@ class Admin::DashboardsController < Admin::AdminController
     @activites_graph_json = []
     activities_graph.each do |item|
       @activites_graph_json << {
-        period: "#{item['month']}/#{item['year']}",
+        period: "#{item['month']}-#{item['year']}",
         activities: item.count
       }
     end
+    matches = {
+      "Outlet" => "Social Media Accounts",
+      "User" => "User"
+    }  
+    activities_pie = PublicActivity::Activity.find_by_sql("
+        SELECT COUNT(id) as count, trackable_type as type FROM activities GROUP BY trackable_type;")
+    @activities_pie_json = []
+    activities_pie.each do |item|
+      @activities_pie_json << { label: matches[item.type], value: item.count}
+    end
+
+   
   end
 
   def social_media_breakdown
