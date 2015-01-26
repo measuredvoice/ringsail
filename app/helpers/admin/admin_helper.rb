aa15406b33cd2ed1e839c016e99c51fa6567f1f4
Justin Raines <justin.magtheridon@gmail.com>
2015-01-26 12:46:19 -0500

    basic timeline added, probably clean up/resize later

---
+++ app/helpers/admin/admin_helper.rb
@@ -0,0 +1,37 @@
+module Admin::AdminHelper
+  def activity_feed_link(activity)
+    case activity.trackable_type
+    when "User"
+    	if activity.trackable
+    		link_to activity.trackable.email, admin_user_path(activity.trackable)
+    	else
+			  "Deleted User"
+    	end
+    when "Outlet"
+    	link_to activity.trackable.organization, admin_outlet_path(activity.trackable)
+    when "MobileApp"
+    	link_to activity.trackable.name, admin_mobile_app_path(activity.trackable)
+    end
+  end
+  def activity_feed_badge_item(activity)
+    case activity.trackable_type
+    when "User"
+      "fa fa-user"
+    when "Outlet"
+      "fa fa-share-alt"
+    when "MobileApp"
+      "fa fa-mobile"    
+    end
+  end
+  def activity_feed_action_name(activity)
+  	t(activity.key).downcase
+  end
+
+  def activity_feed_owner(activity)
+  	if activity.owner
+  		link_to " #{activity.owner.email}".html_safe, admin_user_path(activity.owner)
+    else
+    	"System"
+    end
+  end
+end
