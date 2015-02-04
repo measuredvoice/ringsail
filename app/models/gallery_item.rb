c20b21fec973b4faff47a23866581ea6c3888d6e
Justin Raines <justin.magtheridon@gmail.com>
2015-02-04 12:06:38 -0500

    improve swagger docs, and query ability for galleries/mobile apps

--- app/models/gallery_item.rb
+++ app/models/gallery_item.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: gallery_items
+#
+#  id         :integer          not null, primary key
+#  gallery_id :integer
+#  item_id    :integer
+#  item_type  :string(255)
+#  item_order :integer          default("0")
+#
+
 class GalleryItem < ActiveRecord::Base
   belongs_to :gallery
   belongs_to :item, polymorphic: true
