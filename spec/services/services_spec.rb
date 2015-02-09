cc35e5380317105532eea2d4c9fedc6bdcd62c64
Justin Raines <justin.magtheridon@gmail.com>
2015-02-09 13:18:42 -0500

    adding missing files for services specs

---
+++ spec/services/services_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Service do
+  it "should each define a shortname" do
+    Service.all.each do |service|
+      expect(service.shortname).to be_a_kind_of(Symbol)
+    end
+  end
+
+  it "should define a longname per service"do
+    Service.all.each do |service|
+      expect(service.longname).to be_a_kind_of(String)
+    end
+  end
+
+  it "should define a service url example per service"do
+    Service.all.each do |service|
+      expect(service.service_url_example).to be_a_kind_of(String)
+    end
+  end
+end
---
+++ spec/services/twitter_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe TwitterService do
+  it "should define a shortname" do
+    shortname = TwitterService.shortname
+    expect(shortname).to eq(:twitter)
+  end
+
+end
