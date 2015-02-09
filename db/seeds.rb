e6469f20ea44efbde795f4da257e77a3c3efbcd2
Justin Raines <justin.magtheridon@gmail.com>
2015-02-09 11:05:42 -0500

    improved seeds file to build more cleanly and use faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,109 +1,95 @@
 PublicActivity.enabled = false
-Agency.destroy_all
-User.destroy_all
-Outlet.destroy_all
-OfficialTag.destroy_all
-Gallery.destroy_all
-MobileApp.destroy_all
+DatabaseCleaner.clean
 
-Agency.create!([
-	{
-	name: "Agency of Oops We Did It Again",
-	shortname: "AOWDIA"
-	},
-	{
-	name: "Administration of It Happens",
-	shortname: "AOIH"
-	},
-	{
-	name: "National Zombie Population Control",
-	shortname: "NZPC"	
-	}
-	])
+AGENCIES_NUM = 5
+USERS_NUM = 5 #users will always make a minimum of 4 (one for each role)
+TAGS_NUM = 5
+OUTLETS_NUM = 5
+APPS_NUM = 5
 
-User.create!([
-	{
-	email: "tonymontana@email.com",
-	sign_in_count: "100",
-	user: "scarface",
-	agency_id: Agency.second.id,
-	phone: "1111111111",
-	first_name: "Tony",
-	last_name: "Montana"
-	},
-	{
-	email: "brucewayne@email.com",
-	sign_in_count: "10",
-	user: "Batman",
-	agency_id: Agency.first.id,
-	phone: "2222222222",
-	first_name: "Bruce",
-	last_name: "Wayne"
-	},
-	{
-	email: "harveyspector@email.com",
-	sign_in_count: "20",
-	user: "Harvey",
-	agency_id: Agency.third.id,
-	phone: "3333333333",
-	first_name: "Harvey",
-	last_name: "Spector"
-	},
-	{
-	email: "mikeross@email.com",
-	sign_in_count: "2",
-	user: "Mike",
-	agency_id: Agency.second.id,
-	phone: "5555555555",
-	first_name: "Bruce",
-	last_name: "Wayne"
-	},
-	{
-	email: "frankunderwood@email.com",
-	sign_in_count: "1",
-	user: "Frank",
-	agency_id: Agency.first.id,
-	phone: "1234567890",
-	first_name: "Frank",
-	last_name: "Underwood"
-	}
-	])
+puts "Adding agencies with count of #{AGENCIES_NUM}"
+(1..AGENCIES_NUM).each do |agency_number|
+	Agency.create!({
+		name: Faker::Company.name,
+		shortname: "#{Faker::Company.suffix} #{agency_number}"
+	})
+end
+puts "Finished addingagencies with count of #{AGENCIES_NUM}"
 
-Outlet.create!([
-	{
-	service_url: "http://facebook.com/raddit",
-	organization: "Raddit Org",
-	info_url: "http://raddit.gov",
-	account: "raddit",
-	language: "English",
-	service: "facebook"
-	},
-	{
-	service_url: "http://facebook.com/foo",
-	organization: "Foo Org",
-	info_url: "http://foo.gov",
-	account: "foo",
-	language: "English",
-	service: "facebook"
-	}
-	])
+puts "Create users with each role"
+User.roles.each do |role|
+	User.create!({
+		email: Faker::Internet.email,
+		sign_in_count: rand(1..100),
+		user: Faker::Internet.user_name,
+		agency_id: rand(1..AGENCIES_NUM),
+		phone: Faker::PhoneNumber.phone_number,
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		role: role[1]
+	})
+end
+puts "Finished with users for roles"
+user_count = User.all.count
+if user_count < USERS_NUM
+	puts "Adding additional users to reach count of #{USERS_NUM}"
+	(user_count..USERS_NUM).each do |user_number|
+		role = User.roles.values.sample
+		User.create!({
+			email: Faker::Internet.email,
+			sign_in_count: rand(1..100),
+			user: Faker::Internet.user_name,
+			agency_id: rand(1..AGENCIES_NUM),
+			phone: Faker::PhoneNumber.phone_number,
+			first_name: Faker::Name.first_name,
+			last_name: Faker::Name.last_name,
+			role: role
+		})
+	end
+	puts "Finished adding additional users to reach count of #{USERS_NUM}"
+end
 
-OfficialTag.create!([
-	{
-	tag_text: "stayhome",
-	gallery_count: "0",
-	mobile_app_count: "0",
-	outlet_count: "0"
-	},
-	{
-	tag_text: "commute",
-	gallery_count: "0",
-	mobile_app_count: "0",
-	outlet_count: "0"
-	}
-	])
+(1..TAGS_NUM).each do |tag_number|
+	OfficialTag.create!({
+		tag_text: "#{Faker::Lorem.word} #{tag_number}"
+	})
+end
+
+(1..OUTLETS_NUM).each do |outlet_number|
+	random_service = Service.all.sample
+	language = ["English","Spanish"].sample
+	status = Outlet.statuses.values.sample
+	Outlet.create!({
+		service: random_service.shortname,
+		service_url: random_service.service_url_example,
+		info_url: Faker::Internet.url,
+		account: Faker::Internet.user_name,
+		short_description: Faker::Lorem.sentence,
+		long_description: Faker::Lorem.paragraph,
+		language: language,
+		status: status,
+		agency_ids: [Agency.offset(rand(Agency.count)).first],
+		official_tags: [OfficialTag.offset(rand(OfficialTag.count)).first],
+		users: [User.offset(rand(User.count)).first]
+	})
+end
 
 
+(1..APPS_NUM).each do |app_number|
+	language = ["English","Spanish"].sample
+	status = MobileApp.statuses.values.sample
+	MobileApp.create!({
+		name: Faker::App.name,
+		short_description: Faker::Lorem.sentence,
+		long_description: Faker::Lorem.paragraph,
+		icon_url: Faker::Company.logo,
+		language: language,
+		status: status,
+		agencies: [Agency.offset(rand(Agency.count)).first],
+		official_tags: [OfficialTag.offset(rand(OfficialTag.count)).first],
+		users: [User.offset(rand(User.count)).first]
+	})
+end
 
 
 
