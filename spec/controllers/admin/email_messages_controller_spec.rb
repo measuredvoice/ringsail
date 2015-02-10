f7a2ba74b0c747c84a4609970b27500114f542c8
Michael Gitim <mgitim@ctacorp.com>
2015-02-10 09:59:03 -0500

    add specs for users, emails, and agencies

---
+++ spec/controllers/admin/email_messages_controller_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+RSpec.describe Admin::EmailMessagesController, type: :controller do
+  
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code for admin users" do
+      sign_in FactoryGirl.create(:admin_user)
+      email_message = FactoryGirl.create(:email_message)
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "should redirect users who are not admins" do
+      sign_in FactoryGirl.create(:banned_user)
+      get :index
+      expect(response).to redirect_to(admin_about_url)
+
+      sign_in FactoryGirl.create(:limited_user)
+      get :index
+      expect(response).to redirect_to(admin_about_url)
+
+      sign_in FactoryGirl.create(:full_user)
+      get :index
+      expect(response).to redirect_to(admin_about_url)
+    end
+
+    it "should render the index view" do
+      sign_in FactoryGirl.create(:admin_user)
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "load all email messages" do 
+      build_emails = build_list(:email_message,20)
+      sign_in FactoryGirl.create(:admin_user)
+      get :index
+      expect(assigns(:email_messages)).to match_array(EmailMessage.all)
+    end
+  end
+
+   describe "POST #create" do
+    it "responds successfully with an HTTP 200 status code for admin users" do
+      sign_in FactoryGirl.create(:admin_user)
+      email_message = FactoryGirl.attributes_for(:email_message)
+      post :create, email_message: email_message
+      expect(response).to redirect_to(admin_email_messages_url)
+    end
+
+    it "should redirect users who are not admins" do
+      sign_in FactoryGirl.create(:banned_user)
+      email_message = FactoryGirl.create(:email_message)
+      post :create, email_message: email_message
+      expect(response).to redirect_to (admin_about_url)
+
+      sign_in FactoryGirl.create(:limited_user)
+      email_message = FactoryGirl.create(:email_message)
+      post :create, email_message: email_message
+      expect(response).to redirect_to (admin_about_url)
+
+      sign_in FactoryGirl.create(:full_user)
+      email_message = FactoryGirl.create(:email_message)
+      post :create, email_message: email_message
+      expect(response).to redirect_to (admin_about_url)
+    end
+  end
+
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code for admin users" do
+      sign_in FactoryGirl.create(:admin_user)
+      email_message = FactoryGirl.create(:email_message)
+      get :new
+      expect(response).to render_template("new")
+    end
+
+    it "should redirect users who are not admins" do
+      sign_in FactoryGirl.create(:banned_user)
+      email_message = FactoryGirl.create(:email_message)
+      get :new
+      expect(response).to redirect_to (admin_about_url)
+
+      sign_in FactoryGirl.create(:limited_user)
+      email_message = FactoryGirl.create(:email_message)
+      get :new
+      expect(response).to redirect_to (admin_about_url)
+
+      sign_in FactoryGirl.create(:full_user)
+      email_message = FactoryGirl.create(:email_message)
+      get :new
+      expect(response).to redirect_to (admin_about_url)
+    end
+  end
+
+
+end
--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
-RSpec.describe Admin::AgenciesController, type: :controller do
+RSpec.describe Admin::UsersController, type: :controller do
   
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code for admin users" do
       sign_in FactoryGirl.create(:admin_user)
+      user = FactoryGirl.create(:user)
       get :index
       expect(response).to be_success
       expect(response).to have_http_status(200)
@@ -11,60 +12,66 @@ RSpec.describe Admin::AgenciesController, type: :controller do
 
     it "should redirect users who are not admins" do
       sign_in FactoryGirl.create(:banned_user)
+      user = FactoryGirl.create(:user)
       get :index
       expect(response).to redirect_to(admin_about_url)
 
       sign_in FactoryGirl.create(:limited_user)
+      user = FactoryGirl.create(:user)
       get :index
       expect(response).to redirect_to(admin_about_url)
 
       sign_in FactoryGirl.create(:full_user)
+      user = FactoryGirl.create(:user)
       get :index
       expect(response).to redirect_to(admin_about_url)
     end
 
     it "should render the index view" do
       sign_in FactoryGirl.create(:admin_user)
+      user = FactoryGirl.create(:user)
       get :index
       expect(response).to render_template("index")
     end
 
-    it "load all agencies" do 
-      build_agencies = build_list(:agency,20)
+    it "load all users" do 
+      build_users = build_list(:user,20)
       sign_in FactoryGirl.create(:admin_user)
       get :index
-      expect(assigns(:agencies)).to match_array(Agency.all)
+      expect(assigns(:users)).to match_array(User.all)
     end
   end
 
   describe "GET #show/:id" do
     it "responds successfully with an HTTP 200 status code for admin users" do
       sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.create(:agency)
-      get :show, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "should redirect users who are not admins" do
       sign_in FactoryGirl.create(:banned_user)
-      agency = FactoryGirl.create(:agency)
-      get :show, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to redirect_to (admin_about_url)
 
       sign_in FactoryGirl.create(:limited_user)
-      get :show, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to redirect_to (admin_about_url)
 
       sign_in FactoryGirl.create(:full_user)
-      get :show, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to redirect_to (admin_about_url)
     end
 
     it "should render the show view" do
       sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.create(:agency)
-      get :show, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to render_template("show")
     end
   end
@@ -72,31 +79,33 @@ RSpec.describe Admin::AgenciesController, type: :controller do
   describe "GET #edit/:id" do
     it "responds successfully with an HTTP 200 status code fo admin users" do
       sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.create(:agency)
-      get :edit, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "should redirect users who are not admins" do
       sign_in FactoryGirl.create(:banned_user)
-      agency = FactoryGirl.create(:agency)
-      get :edit, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
       expect(response).to redirect_to (admin_about_url)
 
       sign_in FactoryGirl.create(:limited_user)
-      get :edit, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
       expect(response).to redirect_to (admin_about_url)
 
       sign_in FactoryGirl.create(:full_user)
-      get :edit, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
       expect(response).to redirect_to (admin_about_url)
     end
 
     it "should render the edit view" do
       sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.create(:agency)
-      get :edit, id: agency.id
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
       expect(response).to render_template("edit")
     end
   end
@@ -104,23 +113,25 @@ RSpec.describe Admin::AgenciesController, type: :controller do
   describe "POST #create" do
     it "responds successfully with an HTTP 200 status code for admin users" do
       sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.attributes_for(:agency)
-      post :create, agency: agency
-      expect(response).to redirect_to(admin_agency_path(assigns(:agency)))
+      user = FactoryGirl.attributes_for(:user)
+      post :create, user: user
+      expect(response).to redirect_to(admin_user_path(assigns(:user)))
     end
 
     it "should redirect users who are not admins" do
       sign_in FactoryGirl.create(:banned_user)
-      agency = FactoryGirl.create(:agency)
-      post :create, agency: agency
+      user = FactoryGirl.create(:user)
+      post :create, user: user
       expect(response).to redirect_to (admin_about_url)
 
       sign_in FactoryGirl.create(:limited_user)
-      post :create, agency: agency
+      user = FactoryGirl.attributes_for(:user)
+      post :create, user: user
       expect(response).to redirect_to (admin_about_url)
 
       sign_in FactoryGirl.create(:full_user)
-      post :create, agency: agency
+      user = FactoryGirl.attributes_for(:user)
+      post :create, user: user
       expect(response).to redirect_to (admin_about_url)
     end
   end
@@ -128,14 +139,14 @@ RSpec.describe Admin::AgenciesController, type: :controller do
   describe "GET #new" do
     it "responds successfully with an HTTP 200 status code for admin users" do
       sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.create(:agency)
+      user = FactoryGirl.create(:user)
       get :new
       expect(response).to render_template("new")
     end
 
     it "should redirect users who are not admins" do
       sign_in FactoryGirl.create(:banned_user)
-      agency = FactoryGirl.create(:agency)
+      user = FactoryGirl.create(:user)
       get :new
       expect(response).to redirect_to (admin_about_url)
 
@@ -149,31 +160,5 @@ RSpec.describe Admin::AgenciesController, type: :controller do
     end
   end
 
-  describe "PUT #update/:id" do
-    it "responds successfully with an HTTP 200 status code for admin users" do
-      sign_in FactoryGirl.create(:admin_user)
-      agency = FactoryGirl.attributes_for(:agency, :name => "abcdedfh")
-      put :update, agency: agency
-      response.should be_success
-      expect(response).to redirect_to(admin_agency_path(assigns(:agency)))
-     end
-
-    it "should redirect users who are not admins" do
-      sign_in FactoryGirl.create(:banned_user)
-      agency = FactoryGirl.create(:agency)
-      put :update, id: agency.id
-      expect(response).to redirect_to (admin_about_url)
-
-      sign_in FactoryGirl.create(:limited_user)
-      agency = FactoryGirl.create(:agency)
-      put :update, id: agency.id
-      expect(response).to redirect_to (admin_about_url)
-
-      sign_in FactoryGirl.create(:full_user)
-      agency = FactoryGirl.create(:agency)
-      put :update, id: agency.id
-      expect(response).to redirect_to (admin_about_url)
-    end
-  end
 
 end
