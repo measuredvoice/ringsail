23b8ad6e797b59eab46fd249757e1cfc37e350ab
Justin Raines <justin.magtheridon@gmail.com>
2015-03-11 11:27:38 -0400

    prevent show page from breaking for mobile_apps

--- app/controllers/admin/mobile_apps_controller.rb
+++ app/controllers/admin/mobile_apps_controller.rb
@@ -46,6 +46,8 @@ class Admin::MobileAppsController < Admin::AdminController
   # GET /mobile_apps/new
   def new
     @mobile_app = MobileApp.new
+    @mobile_app.agencies << current_user.agency
+    @mobile_app.users << current_user
     @mobile_app.mobile_app_versions.build
   end
 
