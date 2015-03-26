f7f688df2dcdab824451a6647584ffbea9efebc6
Justin Raines <justin.magtheridon@gmail.com>
2015-03-26 12:33:02 -0400

    fixes android/itunes stores, allows publish date to comet hrough

--- app/models/app_store.rb
+++ app/models/app_store.rb
@@ -21,11 +21,15 @@ class AppStore
     full_uri = "https://play.google.com/store/apps/details?id=#{id}"
     begin
       response = Nokogiri::HTML(open(full_uri).read)
+      date = Date.parse(response.xpath('//*[@itemprop="datePublished"]/text()').to_s.strip)
       details = {
         platform: "Android",
         version: response.xpath('//*[@itemprop="softwareVersion"]/text()').to_s.strip,
         average_rating: response.xpath('//*[@itemprop="aggregateRating"]//meta[@itemprop="ratingValue"]/@content').to_s.strip,
         number_of_ratings: response.xpath('//*[@itemprop="aggregateRating"]//meta[@itemprop="ratingCount"]/@content').to_s.strip,
+        publish_day: date.day,
+        publish_month: date.month,
+        pusbliH_year: date.year,
         screenshot: response.xpath('//img[@itemprop="screenshot"]/@src').map(&:to_s).join("\n")
       }
     rescue Exception => e
@@ -40,13 +44,17 @@ class AppStore
     begin
       response = JSON.parse(open(full_uri).read)
       if response["resultCount"] == 1
+        date = Date.parse(response["results"][0]["releaseDate"])
         return {
           platform: "iOS",
-          version: response["version"],
-          average_rating: response["averageUserRating"],
-          number_of_ratings: response["userRatingCount"],
-          screenshot: response["screenshotUrls"].join("\n"),
-          language: response["language"] == "EN" ? "English" : "Spanish"
+          version: response["results"][0]["version"],
+          average_rating: response["results"][0]["averageUserRating"],
+          number_of_ratings: response["results"][0]["userRatingCount"],
+          publish_day: date.day,
+          publish_month: date.month,
+          publish_year: date.year,
+          screenshot: response["results"][0]["screenshotUrls"].join("\n"),
+          language: response["results"][0]["language"] == "EN" ? "English" : "Spanish"
         }
       else
         return {}
