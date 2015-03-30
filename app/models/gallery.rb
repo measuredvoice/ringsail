5a7c04bb20179b08dd10daf30303a4c35d6aac35
Justin Raines <justin.magtheridon@gmail.com>
2015-03-30 15:21:42 -0400

    gallery view is now the same'

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -63,7 +63,7 @@ class Gallery < ActiveRecord::Base
     CSV.generate(options) do |csv|
       csv << (column_names + ["agencies" ,"contacts" ,"tags"])
 
-      self.all.includes(:agencies,:users,:official_tags).each do |outlet|
+      self.includes(:agencies,:users,:official_tags).each do |outlet|
         csv << (outlet.attributes.values_at(*column_names) + [outlet.agencies.map(&:name).join("|") ,outlet.users.map(&:email).join("|"),outlet.official_tags.map(&:tag_text).join("|")])
       end
     end
