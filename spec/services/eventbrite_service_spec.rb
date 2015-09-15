b97d8ebab2e74afc073a43d21a5a5f656f5b4932
Justin Raines <justin.magtheridon@gmail.com>
2015-09-15 11:04:50 -0400

    adds eventbrite, hulu, yelp

--- spec/services/eventbrite_service_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe EventbriteService do
-  it "should define a shortname" do
-    shortname = EventbriteService.shortname
-    expect(shortname).to eq(:eventbrite)
-  end
-
-end
--- spec/services/hulu_service_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe HuluService do
-  it "should define a shortname" do
-    shortname = HuluService.shortname
-    expect(shortname).to eq(:hulu)
-  end
-
-end
