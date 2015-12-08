03980132dc0fcab80d8ec8a8ad5bc39ebeaa3fe4
Justin Raines <justin.magtheridon@gmail.com>
2015-12-08 13:52:00 -0500

    changes for language list

--- test.rb
+++
@@ -1,8 +0,0 @@
-lines = File.readlines("language_list.txt")
-
-output = File.new("output.txt", "w+")
-
-lines.each do |line|
-  elements = line.split("|")
-  output.write "\"#{elements[3]}\",\n\r"
-end
