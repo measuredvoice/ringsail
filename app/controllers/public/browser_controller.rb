cce1f92733492d8d009ddc1ae9449725fc1632f2
Justin Raines <justin.magtheridon@gmail.com>
2016-10-20 15:45:30 -0400

    ensure languages are filtered in csv results

--- app/controllers/public/browser_controller.rb
+++ app/controllers/public/browser_controller.rb
@@ -9,8 +9,8 @@ class Public::BrowserController < Public::PublicController
     if params[:hidden_tags] && params[:hidden_tags] != ""
       @outlets = @outlets.where("official_tags.id" => params[:hidden_tags].split(","))
     end
-    if params[:hidden_language] && params[:hidden_language] != ""
-      @outlets = @outlets.where("language LIKE ? ", "%#{params[:hidden_language]}%")
+    if params[:hidden_languages] && params[:hidden_languages] != ""
+      @outlets = @outlets.where("language LIKE ? ", "%#{params[:hidden_languages]}%")
     end
     if params[:hidden_services] && params[:hidden_services] != ""
       @outlets = @outlets.where(service: params[:hidden_services].split(","))
@@ -26,8 +26,8 @@ class Public::BrowserController < Public::PublicController
     if params[:hidden_mobile_agencies] && params[:hidden_mobile_agencies] != ""
       @mobile_apps = @mobile_apps.where("agencies.id" =>params[:hidden_mobile_agencies].split(","))
     end
-    if params[:hidden_mobile_language] && params[:hidden_mobile_language] != ""
-      @mobile_apps = @mobile_apps.where("language LIKE ? ", "%#{params[:hidden_mobile_language]}%")
+    if params[:hidden_mobile_languages] && params[:hidden_mobile_languages] != ""
+      @mobile_apps = @mobile_apps.where("language LIKE ? ", "%#{params[:hidden_mobile_languages]}%")
     end
     if params[:hidden_mobile_tags] && params[:hidden_mobile_tags] != ""
       @mobile_apps = @mobile_apps.where("official_tags.id" => params[:hidden_mobile_tags].split(","))
