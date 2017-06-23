a786dec31bef589c694a36cb6662d0c3c2393685
Justin Raines <justin.magtheridon@gmail.com>
2017-06-23 15:02:40 -0400

    first pass at cleaner ui

--- app/controllers/admin/social_media_controller.rb
+++ app/controllers/admin/social_media_controller.rb
@@ -9,21 +9,6 @@ class Admin::SocialMediaController < Admin::AdminController
   # GET /outlets
   # GET /outlets.json
   def index
-    # @outlets = Outlet.includes(:official_tags,:agencies,:users).references(:official_tags,:agencies,:users).where("draft_id IS NULL")
-    # num_items = items_per_page_handler
-    # @total_outlets = @outlets.count
-    # if(params[:service] && !params[:service].blank?)
-    #   @outlets = @outlets.where(service: params[:service])
-    # end
-    # @services = @outlets.group(:service).count
-
-    # respond_to do |format|
-    #   format.html { @outlets = [] }
-    #   format.json {
-    #     @outlets = @outlets.select([:id,:service,:organization,:account,:status,:updated_at])
-    #     render "index" }
-    #   format.csv { send_data @outlets.to_csv }
-    # end
     respond_to do |format|
       format.html { 
         @outlets = Outlet.includes(:official_tags,:agencies,:users).references(:official_tags,:agencies,:users).where("draft_id IS NULL")
