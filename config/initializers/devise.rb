1c76314fde0a8fde9dc65f7168f9943b6af99a0b
Justin Raines <justraines@gmail.com>
2019-10-17 12:55:28 -0400

    adding changes to env variables and ruby version

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -46,8 +46,8 @@ Devise.setup do |config|
   # }
   config.omniauth :login_dot_gov, {
     name: :login_dot_gov,
-    client_id: 'urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:usdr_dev', # same value as registered in the Partner Dashboard
-    idp_base_url: 'https://idp.int.identitysandbox.gov/', # login.gov sandbox environment IdP
+    client_id: ENV['REGISTRY_IDP_CLIENT_ID'], # same value as registered in the Partner Dashboard
+    idp_base_url: ENV['REGISTRY_IDP'], # login.gov sandbox environment IdP
     ial: 1,
     private_key: OpenSSL::PKey::RSA.new(File.read('config/private.pem')),
     redirect_uri: ENV['REGISTRY_LOGIN_REDIRECT'],
