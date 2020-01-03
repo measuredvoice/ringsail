15fd8a26df4ba48a465e7b5d452e3d70af388df9
Justin Raines <justraines@gmail.com>
2020-01-03 09:10:16 -0500

    update the from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,14 +87,18 @@ class User < ActiveRecord::Base
   # end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+    if auth.info.email.end_with? ".gov" || auth.info.email.end_with? ".mil"
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.name = auth.info.name   # assuming the user model has a name
+        user.image = auth.info.image # assuming the user model has an image
+        # If you are using confirmable and the provider(s) you use validate emails, 
+        # uncomment the line below to skip the confirmation emails.
+        # user.skip_confirmation!
+      end
+    else
+      return nil
     end
   end
 
