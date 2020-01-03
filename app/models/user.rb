ed049df4cf23096fea9f810ac773db7cee0676be
Justin Raines <justraines@gmail.com>
2020-01-03 15:48:46 -0500

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,7 +87,7 @@ class User < ActiveRecord::Base
   # end
 
   def self.from_omniauth(auth)
-    if auth.info.email.end_with?(".gov") || auth.info.email.end_with?(".mil")
+    if(auth.info.email.end_with?(".gov") || auth.info.email.end_with?(".mil"))
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
