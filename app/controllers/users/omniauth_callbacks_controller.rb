f3d8c29e73994aac39633cc9945c6da21f86858b
Justin Raines <justraines@gmail.com>
2020-01-09 15:47:34 -0500

    create user if .gov or .mil

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,15 @@ module Users
 
       # Can't find an account, tell user to contact login.gov team
       else
-        redirect_to admin_about_path, status: 302, notice: "Your account could not be found.  Please contact the administrators."
+        if(omniauth_info['email'].end_with?(".gov") || omniauth_info['email'].end_with?(".mil"))
+          User.create({
+            email: omniauth_info['email'],
+            first_name: omniauth_info['given_name'],
+            last_name: omniauth_info['family_name']
+          })
+        else
+            redirect_to admin_about_path, status: 302, notice: "Your account could not be found.  Please contact the administrators."
+        end
       end
     end
 
